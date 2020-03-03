7e3e13f39e55ef3e30038136b6d2e77dcf99f9fa
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-03-03 18:02:43 +0530

    updatin docker

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ RUN pip install nbconvert
 # Running the nbconvert script
 RUN jupyter nbconvert --to script SampleModelGeneratorScriptCopy.ipynb
 RUN python SampleModelGeneratorScriptCopy.py
+RUN python modelUpload.py
 
 ENTRYPOINT ["python"]
 CMD ["app.py"]
--- modelUpload.py
+++ modelUpload.py
@@ -1,11 +1,15 @@
 import requests
 import uuid
-import random 
+import random
+import os
 title = random.choice(["Linear Regression", "Ordinal Regression"])
 author = random.choice( ["Mitesh", "Ashish", "Shashi"])
 idval = random.randrange(10, 20, 2)
 
 import sys
+
+input_datUrl = os.environ['TRAINING_DATA_URL']
+
 pythonVer = sys.version[:5]
 requirementsList = open('requirements.txt').readlines()
 requirements = ' '.join([req.split()[0] for req in requirementsList])
@@ -13,7 +17,7 @@ requirements = ' '.join([req.split()[0] for req in requirementsList])
 description = 'Python v'+pythonVer+' '+requirements
     
 payload = [('fileName','model'+str(uuid.uuid4())),('id',str(idval)),('author',author),('title',title),('description',description),('link','https://52.165.161.123:8000/user/ashkuma/notebooks/demo/ContainerSpinUpRepo/SampleModelGeneratorScriptCopy.ipynb'),('tags','regression'),
-          ('trainingData',"https://genpurposestorage.blob.core.windows.net/imagescontainer/TrainingData2.csv")]
+          ('trainingData',input_datUrl)]
 
 from urllib.parse import urlencode
 params = urlencode(payload)
