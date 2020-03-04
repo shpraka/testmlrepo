7e549cc0318f53c05d1223d9021616eaf5ce46a1
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-03-04 10:22:49 +0530

    updating logic to add notebook link

--- modelUpload.py
+++ modelUpload.py
@@ -2,23 +2,28 @@ import requests
 import uuid
 import random
 import os
+from pathlib import Path
+from os.path import dirname, abspath
 title = random.choice(["Linear Regression", "Ordinal Regression"])
 author = random.choice( ["Mitesh", "Ashish", "Shashi"])
 idval = random.randrange(10, 20, 2)
 
 import sys
 
-
-#input_datUrl = os.getenv("TRAINING_DATA_URL", default="https://genpurposestorage.blob.core.windows.net/imagescontainer/TrainingData2.csv")
 input_datUrl = os.environ.get('TRAINING_DATA_URL', 'https://genpurposestorage.blob.core.windows.net/imagescontainer/TrainingData2.csv')
-print(input_datUrl)
+
 pythonVer = sys.version[:5]
 requirementsList = open('requirements.txt').readlines()
 requirements = ' '.join([req.split()[0] for req in requirementsList])
 
 description = 'Python v'+pythonVer+' '+requirements
-    
-payload = [('fileName','model'+str(uuid.uuid4())),('id',str(idval)),('author',author),('title',title),('description',description),('link','https://52.165.161.123:8000/user/ashkuma/notebooks/demo/ContainerSpinUpRepo/SampleModelGeneratorScriptCopy.ipynb'),('tags','regression'),
+
+modelUploadBase = "https://52.165.161.123:8000/user/ashkuma/notebooks/demo/"
+directory = os.path.split(os.getcwd())[1]
+fileName = "/SampleModelGeneratorScriptCopy.ipynb"
+filePath = modelUploadBase + directory + fileName
+
+payload = [('fileName','model'+str(uuid.uuid4())),('id',str(idval)),('author',author),('title',title),('description',description),('link',filePath),('tags','regression'),
           ('trainingData',input_datUrl)]
 
 from urllib.parse import urlencode
